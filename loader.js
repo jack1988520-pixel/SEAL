9845b9a88a072bc46e9e2eb745724beb0b48a13b
jack1988520-pixel <jack1988520@gmail.com>
2026-08-17 12:07:39 +0800

    Remove SEAL installation code from loader.js
    Removed SEAL installation check and event listener.

--- loader.js
+++ loader.js
@@ -1,12 +1,3 @@
-// 在 loader.js / user.js 頂部注入
-window.__SEAL_INSTALLED = true;
-
-// 回應 Launcher 頁面的動態檢測請求
-window.addEventListener('SEAL_PING_REQUEST', () => {
-    window.dispatchEvent(new CustomEvent('SEAL_PING_RESPONSE', {
-        detail: { version: '2.0', ready: true }
-    }));
-});
 fetch('https://raw.githubusercontent.com/jack1988520-pixel/SEAL/refs/heads/main/SEAL?t=' + Date.now())
   .then(r => r.text())
   .then(c => {
